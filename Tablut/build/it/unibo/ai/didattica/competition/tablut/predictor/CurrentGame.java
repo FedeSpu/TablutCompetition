eae4e45b85f7f8f9012870ecce4bc6751acdbd45
FedeSpu <federico.spurio@studio.unibo.it>
2020-11-15 18:26:29 +0100

    CheckPossibleMoves

--- Tablut/build/it/unibo/ai/didattica/competition/tablut/predictor/CurrentGame.java
+++ Tablut/build/it/unibo/ai/didattica/competition/tablut/predictor/CurrentGame.java
@@ -8,17 +8,21 @@ import it.unibo.ai.didattica.competition.tablut.domain.State;
 import it.unibo.ai.didattica.competition.tablut.domain.State.Pawn;
 import it.unibo.ai.didattica.competition.tablut.domain.State.Turn;
 
-public class CurrentGame implements Game<WrapperState, Action, State.Turn> {
+
+
+public class CurrentGame implements Game<WrapperState, Action , State.Turn> {
+
 
 	private it.unibo.ai.didattica.competition.tablut.domain.Game rules;
 	private WrapperState initialState;
 	private int numberOfBranches;
 
-	public CurrentGame(WrapperState initialState, it.unibo.ai.didattica.competition.tablut.domain.Game rules) {
-		this.initialState = initialState;
-		this.rules = rules;
-		numberOfBranches = 3;
+	public CurrentGame(WrapperState initialState,it.unibo.ai.didattica.competition.tablut.domain.Game rules) {
+		this.initialState=initialState;
+		this.rules=rules;
+		numberOfBranches=3;
 	}
+
 
 	@Override
 	public WrapperState getInitialState() {
@@ -28,8 +32,8 @@ public class CurrentGame implements Game<WrapperState, Action, State.Turn> {
 	@Override
 	public Turn[] getPlayers() {
 		Turn[] players = new State.Turn[2];
-		players[0] = State.Turn.WHITE;
-		players[1] = State.Turn.BLACK;
+		players[0]=State.Turn.WHITE;
+		players[1]=State.Turn.BLACK;
 		return players;
 	}
 
@@ -45,25 +49,24 @@ public class CurrentGame implements Game<WrapperState, Action, State.Turn> {
 
 	@Override
 	public WrapperState getResult(WrapperState state, Action action) {
-		State risState = null;
-		int numberOfTurn = state.getTurn() + 1;
+		State risState=null;
+		int numberOfTurn=state.getTurn()+1;
 		try {
 			risState = rules.checkMove(state.getState(), action);
 		} catch (Exception e) {
 			System.out.println("Oh oh something wrong in CurrentGame getResult");
-			e.printStackTrace();
 		}
 
-		return new WrapperState(risState, numberOfTurn);
+		return new WrapperState(risState,numberOfTurn);	
 	}
 
 	@Override
 	public boolean isTerminal(WrapperState state) {
-		if (state.getState().getTurn().equalsTurn(State.Turn.BLACKWIN.toString())
+		if (state.getState().getTurn().equalsTurn(State.Turn.BLACKWIN.toString()) 
 				|| state.getState().getTurn().equalsTurn(State.Turn.WHITEWIN.toString())
 				|| state.getState().getTurn().equalsTurn(State.Turn.DRAW.toString())) {
 			return true;
-		} else if (state.getTurn() - initialState.getTurn() >= numberOfBranches) {
+		}else if (state.getTurn()-initialState.getTurn()>=numberOfBranches) {
 			return true;
 		}
 		return false;
@@ -72,12 +75,18 @@ public class CurrentGame implements Game<WrapperState, Action, State.Turn> {
 	@Override
 	public double getUtility(WrapperState state, Turn player) {
 		int ris = -1;
-		if (player.equalsTurn(Turn.BLACK.toString())) {
+		if (player.equalsTurn(Turn.BLACK.toString())){
 			ris = state.getState().getNumberOf(Pawn.WHITE) + state.getState().getNumberOf(Pawn.KING);
-		} else if (player.equalsTurn(Turn.WHITE.toString())) {
+		}else if (player.equalsTurn(Turn.WHITE.toString())) {
 			ris = state.getState().getNumberOf(Pawn.WHITE) + state.getState().getNumberOf(Pawn.BLACK);
 		}
 		return ris;
 	}
+
+
+
+
+
+
 
 }
